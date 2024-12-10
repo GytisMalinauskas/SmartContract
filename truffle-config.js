c1cf0405173505eaf023311fccdc380a077078a1
GytisMalinauskas <pvardenis30@gmail.com>
2024-12-10 14:36:34 +0200

    config changes

--- truffle-config.js
+++ truffle-config.js
@@ -19,6 +19,15 @@ module.exports = {
       network_id: "*", // Match any network ID
     },
   },
+  sepolia: {
+    provider: () =>
+      new HDWalletProvider(
+        process.env.MNEMONIC,
+          `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`      ),
+    network_id: 11155111,
+    gas: 450000,
+    gasPrice: 100000
+  },
   compilers: {
     solc: {
       version: "0.4.18", // Use this specific version
